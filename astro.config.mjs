1def653ce7fe1463f4a0eaf49ae9cc69e950d2b2
vasyaqwe <elekton.fafik@gmail.com>
2024-09-11 10:39:55 +0300

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import react from "@astrojs/react"
+import sitemap from "@astrojs/sitemap"
 import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-   integrations: [react()],
+   site: "https://vasyldev.cc",
+   integrations: [react(), sitemap()],
    server: {
       port: 3000,
    },
